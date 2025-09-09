83ccbe7236433be99c92a357690c0dd3c7e56912
Nico <168318133+NicoPlaysThis@users.noreply.github.com>
2025-09-09 13:37:00 -0600

    Add the proper file

--- content-script.js
+++ content-script.js
@@ -1,19 +1,541 @@
-function addSimplifiedChinese() {
-  const schnElement = document.createElement("a");
-  schnElement.href = "/sets/schn";
-  schnElement.className = "tcg-region-links-button-group-link button button-plain-alt";
-  schnElement.setAttribute("data-tcg-region-id", "3");
-  schnElement.setAttribute("data-link-visitor-disabled", "");
-  schnElement.innerText = "S. Chinese";
+let schnInjected = false;
+
+function injectSimplifiedChinese() {
+  // don't run again if already ran on the page only if the page IS reset
+  if (schnInjected) return;
+  schnInjected = true;
+  //
+
+  // if we are on /sets/schn, delete the "not found" and inject our new page
+  if (window.location.pathname === "/sets/schn") {
+    const notFound = document.querySelector("#page-header");
+    if (notFound) notFound.remove();
+
+    const notFound2 = document.querySelector("#page-content .container");
+    if (notFound2) notFound2.remove();
+
+    const notFound3 = document.querySelector("#page-content");
+
+    const stylesheetSetPage = document.createElement("link");
+    stylesheetSetPage.rel = "stylesheet";
+    stylesheetSetPage.href = "https://static.tcgcollector.com/build/css/page.sets.sets.d26a8807.css";
+    
+    if (!document.querySelector(`link[href="${stylesheetSetPage.href}"]`)) { // avoid duplicates
+      document.head.appendChild(stylesheetSetPage);
+      console.log(`Set Page Stylesheet injected: ${stylesheetSetPage.href} ✅`);
+    }
+
+    if (notFound3) {
+      notFound3.innerHTML = `
+  <div class="container">
+
+    <div id="tcg-region-links-button-group-container">
+
+      <div id="tcg-region-links-button-group" class="button-group ">
+
+        <a href="/sets/intl?setMode=allCardVariants&amp;releaseDateOrder=newToOld&amp;displayAs=logos" class="
+        tcg-region-links-button-group-link
+        button
+        button-plain-alt
+              " data-tcg-region-id="1" data-link-visitor-disabled="">
+          International
+        </a>
+        <a href="/sets/jp?setMode=allCardVariants&amp;releaseDateOrder=newToOld&amp;displayAs=logos" class="
+        tcg-region-links-button-group-link
+        button
+        button-plain-alt
+              " data-tcg-region-id="2" data-link-visitor-disabled="">
+          Japan
+        </a></div>
+
+      <div id="sets-page-buttons">
+
+        <button type="button" title="Share my collection" aria-label="Share my collection" class="
+              card-collection-share-button
+              button
+              button-plain-alt
+              button-with-icon-only
+            " fdprocessedid="s9txl">
+          <span aria-hidden="true" class="button-icon fa-solid fa-share-nodes fa-fw"></span>
+        </button>
+
+        <button type="button" title="Change my preferences" aria-label="Change my preferences" id="sets-page-preferences-drawer-show-button" class="button button-plain-alt button-with-icon-only" fdprocessedid="46k1j2">
+          <span aria-hidden="true" class="button-icon fa-solid fa-gear fa-fw"></span>
+        </button>
+
+      </div>
+
+    </div>
+
+    <div id="sets-page-actions">
+
+      <div id="set-search-form-control-container" class="form-control-container">
+
+        <span aria-hidden="true" class="form-control-icon fa-solid fa-magnifying-glass"></span>
+
+        <button type="button" class="
+            form-control-clear-button
+                      " tabindex="-1">
+        </button>
+
+        <input type="search" id="set-search-form-control" name="setSearch" class="form-control" value="" placeholder="Search sets..." autocomplete="off" size="25">
+
+      </div>
+
+      <div id="set-source-radios-container">
+
+        <div id="set-source-radios">
+
+          <div class="radio set-source-radio">
+
+            <input type="radio" id="radio-input-13582569" name="setSource" class="radio-input" value="all" checked="">
+
+            <label for="radio-input-13582569" class="radio-label">
+              <span aria-hidden="true" class="radio-indicator"></span>
+              All
+            </label>
+
+          </div>
+
+          <div class="radio set-source-radio">
+
+            <input type="radio" id="radio-input-186843698" name="setSource" class="radio-input" value="cardCollectionInProgress">
+
+            <label for="radio-input-186843698" class="radio-label">
+              <span aria-hidden="true" class="radio-indicator"></span>
+              In progress
+            </label>
+
+          </div>
+
+          <div class="radio set-source-radio">
+
+            <input type="radio" id="radio-input-555182996" name="setSource" class="radio-input" value="cardCollectionCompleted">
+
+            <label for="radio-input-555182996" class="radio-label">
+              <span aria-hidden="true" class="radio-indicator"></span>
+              Completed
+            </label>
+
+          </div>
+        </div>
+
+        <div id="set-mode-dropdown-container">
+
+          <label>Show sets as</label>
+
+          <button type="button" class="
+        dropdown-toggle
+        button button-link-like-alt      " data-toggle="dropdown" data-target="#set-mode-dropdown" fdprocessedid="982zng">
+            <span aria-hidden="true" class="dropdown-toggle-caret"></span>
+            All card variants
+          </button>
+
+          <div id="set-mode-dropdown" class="
+    dropdown
+          dropdown-selectable
+          " data-toggle-text-separator=", " data-query-string-key="setMode">
+
+            <div class="dropdown-menu">
+
+              <div class="dropdown-menu-content">
+
+                <div class="dropdown-option " tabindex="0" data-value="anyCardVariant">
+                  Any card variant
+                </div>
+
+                <div class="dropdown-option " tabindex="0" data-value="regularCardVariants">
+                  Regular card variants
+                </div>
+
+                <div class="dropdown-option selected" tabindex="0" data-value="allCardVariants">
+                  All card variants
+                </div>
+
+                <div class="dropdown-divider"></div>
+
+                <div class="dropdown-option " tabindex="0" data-value="standardSet">
+                  Standard set
+                </div>
+
+                <div class="dropdown-option " tabindex="0" data-value="parallelSet">
+                  Parallel set
+                </div>
+
+                <div class="dropdown-divider"></div>
+
+                <a href="/account/settings/card-collection" class="dropdown-option">
+                    <span class="dropdown-option-left-item-container">
+                        <span aria-hidden="true" class="dropdown-option-side-item-icon fa-solid fa-gear fa-fw">
+                        </span>
+                    </span>
+                  Change progress settings
+                </a>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+  <div id="set-search-result" class="
+        set-sort-by-expansion-series
+  " style="--set-logo-reference-aspect-ratio: 2.5;">
+
+    <div class="container">
+
+      <div id="set-search-result-header">
+
+        <div id="set-search-result-title">
+
+        <span id="set-search-result-title-count">
+          1
+        </span>
+
+          <span id="set-search-result-title-name">
+          sets found</span>
+        </div>
+
+        <div id="set-search-result-compact-header-buttons">
+          <button type="button" title="Show the display options" aria-label="Show the display options" class="set-search-result-compact-header-button" data-show="drawer" data-target="#set-display-options-drawer">
+            <span aria-hidden="true" class="fa-solid fa-arrow-down-wide-short"></span>
+          </button>
+        </div>
+
+      </div>
+
+      <div id="set-display-options">
+
+        <div class="set-display-option">
+
+          <label>Sort by</label>
+
+          <button type="button" class="
+        dropdown-toggle
+        button button-link-like-alt      " data-toggle="dropdown" data-target="#dropdown-685576854" fdprocessedid="0wmvjq">
+            <span aria-hidden="true" class="dropdown-toggle-caret"></span>
+            Series
+          </button>
+
+          <div id="dropdown-685576854" class="
+    dropdown
+          dropdown-selectable
+        set-display-option-dropdown  " data-toggle-text-separator=", " data-query-string-key="sortBy">
+
+            <div class="dropdown-menu">
+
+              <div class="dropdown-menu-content">
+
+                <div class="dropdown-option selected" tabindex="0" data-value="expansionSeries">
+                  Series
+                </div>
+
+                <div class="dropdown-option " tabindex="0" data-value="releaseDate">
+                  Release date
+                </div>
+
+                <div class="dropdown-option " tabindex="0" data-value="cardCollectionProgress">
+                  Collection progress
+                </div>
+
+                <div class="dropdown-option " tabindex="0" data-value="marketPriceDesc">
+                  Market price (desc)
+                </div>
+
+                <div class="dropdown-option " tabindex="0" data-value="marketPriceAsc">
+                  Market price (asc)
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="set-display-option">
+
+          <label>From</label>
+
+          <button type="button" class="
+        dropdown-toggle
+        button button-link-like-alt      " data-toggle="dropdown" data-target="#dropdown-2085179571" fdprocessedid="f247hk">
+            <span aria-hidden="true" class="dropdown-toggle-caret"></span>
+            New to old
+          </button>
+
+          <div id="dropdown-2085179571" class="
+    dropdown
+          dropdown-selectable
+        set-display-option-dropdown  " data-toggle-text-separator=", " data-query-string-key="releaseDateOrder">
+
+            <div class="dropdown-menu">
+
+              <div class="dropdown-menu-content">
+
+                <div class="dropdown-option selected" tabindex="0" data-value="newToOld">
+                  New to old
+                </div>
+
+                <div class="dropdown-option " tabindex="0" data-value="oldToNew">
+                  Old to new
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="set-display-option">
+
+          <label>Show</label>
+
+          <button type="button" class="
+        dropdown-toggle
+        button button-link-like-alt      " data-toggle="dropdown" data-target="#dropdown-1168574368" fdprocessedid="ly3olr">
+            <span aria-hidden="true" class="dropdown-toggle-caret"></span>
+            Logos
+          </button>
+
+          <div id="dropdown-1168574368" class="
+    dropdown
+          dropdown-selectable
+        set-display-option-dropdown  " data-toggle-text-separator=", " data-query-string-key="displayAs">
+
+            <div class="dropdown-menu">
+
+              <div class="dropdown-menu-content">
+
+                <div class="dropdown-option " tabindex="0" data-value="list">
+                  List
+                </div>
+
+                <div class="dropdown-option selected" tabindex="0" data-value="logos">
+                  Logos
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div id="expansion-series-nav" class="">
+      <div class="container">
+        <div id="expansion-series-nav-content">
+
+          <button type="button" title="Show all series" aria-label="Show all series" id="expansion-series-nav-drawer-show-button" data-show="drawer" data-target="#expansion-series-nav-drawer" fdprocessedid="mim3ea">
+            <span aria-hidden="true" class="button-icon fa-solid fa-list-ul"></span>
+          </button>
+
+          <button type="button" title="Scroll left" aria-label="Scroll left" id="expansion-series-nav-scroll-left-button" class="hidden">
+            <span aria-hidden="true" class="button-icon fa-solid fa-chevron-left"></span>
+          </button>
+
+          <div id="expansion-series-nav-jump-links">
+            <a href="#gem-pack-era" title="Jump to 'Gem Pack Era'" aria-label="Jump to 'Gem Pack Era'" class="expansion-series-nav-jump-link">
+              Gem Pack Era
+            </a>
+
+          </div>
+
+          <button type="button" title="Scroll right" aria-label="Scroll right" id="expansion-series-nav-scroll-right-button" class="hidden" fdprocessedid="r2fze8">
+            <span aria-hidden="true" class="button-icon fa-solid fa-chevron-right"></span>
+          </button>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div id="set-logo-grids">
+
+
+        <div id="gem-pack-era" class="set-logo-grid">
+
+          <h2 class="set-logo-grid-title">Gem Pack Era</h2>
+
+          <div class="set-logo-grid-items">
+
+
+            <div class="
+              set-logo-grid-item
+              set-has-cards              set-has-code              set-has-symbol            ">
+
+              <div class="set-logo-grid-item-header">
+
+                <img src="https://i.ibb.co/nNXq3v5k/Screenshot-2025-09-04-211507.png" srcset="https://i.ibb.co/nNXq3v5k/Screenshot-2025-09-04-211507.png 25w, https://i.ibb.co/nNXq3v5k/Screenshot-2025-09-04-211507.png 46w" loading="eager" alt="Gem Pack Vol 1" width="25" height="14" sizes="(max-width: 25px) 100vw, 25px" class="set-symbol set-logo-grid-item-set-symbol">
+
+                <span class="set-logo-grid-item-set-name-container">
+
+
+                  <a href="/sets/60000/gem-pack-vol-1" title="Gem Pack Vol 1" class="set-logo-grid-item-set-name">
+                    Gem Pack Vol 1</a><span class="set-logo-grid-item-set-code">CBB1C
+                  </span>
+
+              </span>
+
+              </div>
+
+              <div class="set-logo-grid-item-body">
+
+                <a href="/sets/60000/gem-pack-vol-1" class="set-logo-grid-item-set-logo-container">
+
+                  <img src="https://www.pokekarty.pl/wp-content/uploads/2025/03/Gem-Pack-Volume-1-Simplified-Chinese-Pokemon.png" srcset="https://www.pokekarty.pl/wp-content/uploads/2025/03/Gem-Pack-Volume-1-Simplified-Chinese-Pokemon.png 519w" loading="eager" alt="Gem Pack Vol 1" width="250" height="60" sizes="(max-width: 250px) 100vw, 250px" class="
+                    set-logo-grid-item-set-logo
+                    is-wider-than-reference
+                  ">
+
+
+                </a>
+
+                <div class="set-logo-grid-item-text-content">
+
+                  <div class="set-logo-grid-item-release-date">
+                    Jan 17, 2025
+
+                  </div>
+
+                  <div class="set-logo-grid-item-price">
+
+
+                    <a href="/pluspluserror" rel="external nofollow" target="_blank" title="View card prices" aria-label="View card prices">
+                      $—
+                    </a>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div class="set-logo-grid-item-footer">
+
+                <div class="set-logo-grid-item-status">
+
+
+                  <div class="progress set-logo-grid-item-status-progress" style="--progress-percentage: 1%;">
+                    <div class="progress-label">  1/115
+                    </div>
+                    <div class="progress-percentage">1%</div>
+                    <div aria-hidden="true" class="progress-bar"></div>
+                  </div>
+
+
+                </div>
+
+
+                <button type="button" class="
+    set-like-card-collection-details-drawer-show-button
+
+                  set-logo-grid-item-collection-details-drawer-show-button
+                  button
+                  button-small
+                  button-plain
+
+                  " data-set-like-id="11660" data-set-mode="allCardVariants" fdprocessedid="0rgfv">
+
+                  <span aria-hidden="true" class="button-icon fa-solid fa-chart-simple"></span>
+                  View details
+
+                </button>
+
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="affiliation-text-tcg++">TCG++ is an addon for TCGCollector and is not endorsed nor produced by TCGCollector.</div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</main>
+      `;
+    }
+  }
   
-  const container = document.querySelector(".tcg-region-links-button-group");
-  if (container) {
-    container.appendChild(schnElement);
-  } else {
-    console.log("Container not found, injecting at body instead");
-    document.body.appendChild(schnElement);
+  let RegionContainer = document.querySelector("#tcg-region-links-button-group");
+
+  if (RegionContainer) {
+    const schnLink = document.createElement("a");
+
+    const url = window.location.pathname;
+    if (url.includes("/sets/")) {
+      schnLink.href = "/sets/schn";
+    } else if (url.includes("/dashboard")) {
+      schnLink.href = "/dashboard/schn";
+    } else if (url.includes("/cards")) {
+      schnLink.href = "/cards/schn";
+    } else {
+      schnLink.href = "/pluspluserror";
+    }
+
+    if (window.location.pathname === "/sets/schn") {
+      schnLink.className = "tcg-region-links-button-group-link button button-plain-alt active";
+      console.log("Changed SCHN button to active ✅")
+    } else {
+      schnLink.className = "tcg-region-links-button-group-link button button-plain-alt";
+    }
+
+    console.log(window.location.pathname);
+
+    schnLink.setAttribute("data-tcg-region-id", "3");
+    schnLink.setAttribute("data-link-visitor-disabled", "");
+    schnLink.innerText = "S. Chinese";
+
+    RegionContainer.appendChild(schnLink);
+    console.log("S. Chinese button made (" + schnLink.href + ") ✅");
+
+    if (window.location.pathname === "/sets/schn") {
+      const schnSetsPageTitle = "Pokémon TCG Sets (Simplified Chinese) - TCGCollector";
+      if (document.title !== schnSetsPageTitle) {
+        document.title = schnSetsPageTitle;
+      }
+      console.log("Changed Page Title to " + schnSetsPageTitle + " ✅")
+    }
   }
 }
+
+// run this func once, can set up the MutationObserver if you want
+injectSimplifiedChinese();
+
+// // set up MutationObserver to re-run when inevitably DOM changes
+// const schnObserver = new MutationObserver(() => {
+//   injectSimplifiedChinese();
+// });
+
+// schnObserver.observe(document.body, {
+//   childList: true,
+//   subtree: true,
+// });
 
 function enableDarkMode() {
   const styles = document.createElement('style')
@@ -294,5 +816,5 @@ function attachOrganizerButtons() {
 }
 
 // document.body.style.backgroundColor = 'black'
-enableDarkMode()
+// enableDarkMode()
 attachOrganizerButtons()
